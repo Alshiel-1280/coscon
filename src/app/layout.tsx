e0bdfcd03f5c6ec42bbe114123601e5990467610
Alshiel-1280 <ryosukedaay@gmail.com>
2026-02-12 18:37:06 +0900

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: APP_NAME,
   description: `${APP_NAME} | Cosplay storyboard app`,
+  verification: {
+    google: "Uf6VlOuLG1tF3zLo8T90J3pX-thFy4k9fcuvX8QRY30",
+  },
 };
 
 export default function RootLayout({
